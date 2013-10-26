ced0e5ad331870a61644a3617a9e8a97cd652f00
Watson <watson1978@gmail.com>
2013-10-26 18:30:33 +0900

    Add spec

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,9 +1,6 @@
-describe "Application 'motion-env-vars'" do
-  before do
-    @app = UIApplication.sharedApplication
-  end
-
-  it "has one window" do
-    @app.windows.size.should == 1
+describe "ENV_VARS" do
+  it "has YAML entries" do
+    ENV_VARS["age"].should == 33
+    ENV_VARS["name"].should == "John Smith"
   end
 end
